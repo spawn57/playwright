518ce7f4381249e74f2557c76eb693d468b9fe64
Sunil Khiatani <skhiatani@gmail.com>
2023-09-19 21:22:33 +0800

    basic tests done

---
+++ tests/example.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('My tests', () => {
+  test.beforeAll(async () => {
+    console.log("Before tests");
+  });
+
+  test("has title", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test("get started link", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+
+    // Click the get started link.
+    await page.getByRole("link", { name: "Get started" }).click();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(
+      page.getByRole("heading", { name: "Installation" })
+    ).toBeVisible();
+  });
+
+  test("test using a browser context", async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://playwright.dev");
+    let content: string = await page.content();
+
+    expect(content).toContain("Playwright");
+
+    await context.close();
+  });
+
+  test.afterAll(async () => {
+    console.log("after tests");
+  });
+});
